9bc1857ee313ce7d482d50893031cbe0ff4eaaf8
swethambarikv <swethambarikv@gmail.com>
2022-06-16 11:16:47 +0530

    commit

--- nodejs/db.js
+++ nodejs/db.js
@@ -1,10 +1,10 @@
 const mangoose = require('mangoose');
 
-mangoose.connect('mongodb://mongodb://localhost:27017/user', (err) =>{
+mangoose.connect('mongodb://mongodb://localhost:27017/management', (err) =>{
     if(!err)
         console.log('MongoDB connection succeeded');
     else
         console.log('Error in MongoDB connection: ' + JSON.stringify(err,undefined,2));
 
 });
-module.exports = mangoose;
+module.exports = mangoose;  
