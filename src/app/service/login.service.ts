acf3a09887b3c4217a08274f78c230fadeb28d87
swethambarikv <swethambarikv@gmail.com>
2022-06-27 12:17:46 +0530

    27/06/2022

--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -6,45 +6,47 @@ import { User1 } from '../user/user';
   providedIn: 'root'
 })
 export class LoginService {
-  public userLogin=new User1(' ',' ');
+  public userLogin = new User1(' ', ' ');
   public userList: User[] = [{
-    id:1,
+    id: 1,
     name: 'Tharani',
     email: 'tharani@gmail.com',
     gender: 'female',
     mobile: '9360506369',
-    topic:'angular'
-}];
-public id1:number=0;
-public Role:string='';
-  constructor() { }
-public setMessage(user: User){
-    user.id = this.userList.length+1;
+    topic: 'angular'
+  }];
+  public id1: number = 0;
+  public Role: string = '';
+  constructor() { 
+    
+  }
+  public setMessage(user: User) {
+    user.id = this.userList.length + 1;
     this.userList.push(user);
 
   }
-public getUsersByName(name : String){
+  public getUsersByName(name: String) {
     this.userList = this.userList.filter(x => x.name != name);
   }
-public getUsers(){
+  public getUsers() {
     return this.userList
   }
-public getUsersByID(id: number){
+  public getUsersByID(id: number) {
     return this.userList.find(x => x.id == id)
   }
-public removeUser(name : String) {
+  public removeUser(name: String) {
     this.userList = this.userList.filter(x => x.name != name);
-}
-public loggedIn(){
+  }
+  public loggedIn() {
     return true;
   }
-public logged(){
+  public logged() {
     return true;
   }
-public getRole(role:string){
-    this.Role=role;
+  public getRole(role: string) {
+    this.Role = role;
   }
-public updateUser(user:User){
+  public updateUser(user: User) {
     const userIndex = this.userList.findIndex(x => x.id == user.id);
     this.userList[userIndex] = user;
   }
